d6abfbf09fe18439c24d974e94d1ab540284df9f
AIT TR Student <vitali.mazurenka@web.de>
2023-10-16 23:06:22 +0200

    final

---
+++ src/ait/homework/AchillesThread.java
@@ -0,0 +1,15 @@
+package ait.homework;
+
+public class AchillesThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 30; i++) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(Thread.currentThread().getName() + " I run fast");
+        }
+    }
+}
---
+++ src/ait/homework/TortoiseThread.java
@@ -0,0 +1,18 @@
+package ait.homework;
+
+public class TortoiseThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 12; i++) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(Thread.currentThread().getName() + " I just run");
+
+        }
+
+    }
+
+}
